7549b2629253a965674443ce4adfe1c4484d7967
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-07 19:01:46 +0000

    generalize src dir

--- src/src.mk
+++ src/src.mk
@@ -1,18 +1,14 @@
-BUS_OBJ := $(patsubst %.o, $(OBJS_PATH)%.o, util.o BusController.o)
-RT_OBJ := $(patsubst %.o, $(OBJS_PATH)%.o, util.o RT.o)
+##Declare your object files here
+##MYOBJ := $(patsubst %.o, $(OBJS_PATH)%.o, obj1.o obj2.o)
 
-all: BusController RemoteTerminal
-
-BusController: $(BUS_OBJ)
-	$(LINK) -o $(ROOT_PATH)BC.out $(BUS_OBJ)
-
-RemoteTerminal: $(RT_OBJ)
-	$(LINK) -o $(ROOT_PATH)RT.out $(RT_OBJ)
+##Place your source rules here
+#Program: $(RT_OBJ)
+#	$(LINK) -o $(ROOT_PATH)Program.out $(MYOBJ)
 
 $(OBJS_PATH)%.o: $(SRC_PATH)%.c
 	$(COMPILE) -o $@ $<
 
-.PHONY: all clean
+.PHONY: clean
 
 clean:
 	-rm $(OBJS_PATH)*.o $(ROOT_PATH)*.out
