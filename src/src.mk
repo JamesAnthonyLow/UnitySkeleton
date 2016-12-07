5e3034fb911abab32e071c71d883a2b6ffe42229
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-07 19:07:12 +0000

    fix typo in src.mk

--- src/src.mk
+++ src/src.mk
@@ -1,6 +1,6 @@
 ##Declare your object files here
 ##MYOBJ := $(patsubst %.o, $(OBJS_PATH)%.o, obj1.o obj2.o)
-HELLOWORLD_OBJ := $(patsubst %.o, $(OBJS_PATH)%.o, HelloWorld.o, MyProgram.o)
+HELLOWORLD_OBJ := $(patsubst %.o, $(OBJS_PATH)%.o, HelloWorld.o MyProgram.o)
 
 ##Place your source rules here
 #Program: $(RT_OBJ)
