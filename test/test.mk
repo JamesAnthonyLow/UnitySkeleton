ce247fca4deb57ccb648764ef12ac597705ad4b6
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-12 18:24:03 +0000

    remove unnecessary redundant PATH variable declarations

--- test/test.mk
+++ test/test.mk
@@ -1,16 +1,3 @@
-ROOT_PATH = ./
-UNITY_PATH = ./test/Unity/src/
-SRC_PATH = ./src/
-TEST_PATH = ./test/
-BUILD_PATH = ./build/
-OBJS_PATH = ./build/objs/
-OUT_PATH = ./build/out/
-RESULTS_PATH = ./build/results/
-PATHS = $(BUILD_PATH) $(OUT_PATH) $(OBJS_PATH) $(RESULTS_PATH)
-COMPILE = gcc -c
-LINK = gcc
-CFLAGS := -I$(UNITY_PATH) 
-
 TEST_DIRS = $(dir $(wildcard $(TEST_PATH)*/*.c))
 SOURCE_DIRS = $(patsubst $(TEST_PATH)%, $(SRC_PATH)%, $(TEST_DIRS)) 
 TESTS = $(patsubst $(TEST_PATH)%/, %, $(TEST_DIRS))
