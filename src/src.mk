e47a49b40b130145af32acb06a2d6c6f87d13369
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-12 18:43:05 +0000

    define src objs

--- src/src.mk
+++ src/src.mk
@@ -1,15 +1,26 @@
 ##Declare your object files here
 ##MYOBJ := $(patsubst %.o, $(OBJS_PATH)%.o, obj1.o obj2.o)
-HELLOWORLD_OBJ := $(patsubst %.o, $(OBJS_PATH)%.o, HelloWorld.o MyProgram.o)
 
 ##Place your source rules here
 #Program: $(RT_OBJ)
 #	$(LINK) -o $(ROOT_PATH)Program.out $(MYOBJ)
-HelloWorld: $(HELLOWORLD_OBJ)
+HelloWorld: 
 	$(LINK) -o $(ROOT_PATH)HelloWorld.out $(HELLOWORLD_OBJ)
 
 $(OBJS_PATH)%.o: $(SRC_PATH)%.c
 	$(COMPILE) -o $@ $<
+
+HelloWorldObjs:
+	@echo $(HelloWorld_OBJS)
+
+define SRC_DIR_OBJS
+$(1)_OBJS = $(patsubst %.c, %.o, $(wildcard $(SRC_PATH)$(1)/*.c))
+endef
+
+SRC_DIRS = $(dir $(wildcard $(SRC_PATH)*/*.c))
+SRCS = $(patsubst $(SRC_PATH)%/, %, $(SRC_DIRS))
+
+$(foreach d,$(SRCS),$(eval $(call SRC_DIR_OBJS,$(d))))
 
 .PHONY: clean
 
