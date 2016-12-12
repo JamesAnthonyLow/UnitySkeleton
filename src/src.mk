23acacc8f22c1d263d2a63d818082fa7f3fed43c
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-12 20:54:29 +0000

    add MKDIR RULES

--- src/src.mk
+++ src/src.mk
@@ -7,12 +7,23 @@ $(1)Src = $(SRC_PATH)$(1)/
 endef
 
 define SRC_OBJS_RULES
-$($(1)Objs)%.o: $($(1)Src)%.c
-	$(COMPILE) -o $@ $^
+$($(1)Objs)%.o: $(HelloWorldObjs) $($(1)Src)%.c
+	$(COMPILE) -o $$@ $$^
+endef
+
+define MKDIR_RULES
+$($(1)Objs):
+	mkdir -p $$@
+$($(1)Src):
+	mkdir -p $$@
 endef
 
 $(foreach s,$(SRCS),$(eval $(call SRC_PATHS,$(s))))
 $(foreach s,$(SRCS),$(eval $(call SRC_OBJS_RULES,$(s))))
+$(foreach s,$(SRCS),$(eval $(call MKDIR_RULES,$(s))))
 
 Main: $(OBJS_PATH)MyProgram.o $(HelloWorldObjs)Greeting.o
 	$(LINK) -o $@.out $^
+
+$(OBJS_PATH)%.o: $(SRC_PATH)%.c
+	$(COMPILE) -o $@ $^
